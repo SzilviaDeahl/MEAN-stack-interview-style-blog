f5e5266ba7f99118420a9bcd765d304747b33f3f
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-09-30 11:47:44 -0600

    angular and bootstrap successfully set up

---
+++ routes/blog.js
@@ -0,0 +1,16 @@
+// var express = require('express');
+// var router = express.Router();
+// var db = require('monk')('localhost/myBlog');
+// var blogCollection = db.get('blogs')
+//
+// router.get('/', function (req, res, next) {
+//   blogCollection.find({}, function (error, users) {
+//     res.render('index', {blogs: blogs})
+//   })
+// });
+//
+// module.exports = router;
+//
+
+// make an index.html in public directory
+// controller goes into the public
